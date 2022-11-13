2e2b8db1ef864a9ee9fc2b14d48bb2ed735d7371
Maizares, Juan <maizaresjuan.coder@gmail.com>
2022-11-13 19:53:06 -0300

    pantalla de escritorio listo

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,39 @@
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector('button');
+//
+const searchParams = new URLSearchParams( window.location.search);
+
+if (!searchParams.has('escritorio')) {
+    window.location = 'index.html';
+    throw new Error('El escritorio es obligatorio');
+
+}
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText = escritorio;
 
 
-console.log('Escritorio HTML');
+const socket = io();
+
+
+//mtd . Declaracion de canales de eventos.
+socket.on('connect', () => {
+    btnAtender.disabled=false;
+
+});
+
+socket.on('disconnect', () => {
+    btnAtender.disabled=true;
+    
+});
+
+socket.on('ultimo-ticket', (ultimo) => {
+    // lblNuevoTicket.innerText = 'Ticket' + ultimo; 
+})
+
+btnCrear.addEventListener( 'click', () => {
+    
+    // socket.emit( 'siguiente-ticket', null, ( ticket ) => { //ticket es 'siguiente'
+    //     lblNuevoTicket.innerText = ticket;
+    // });
+
+});
